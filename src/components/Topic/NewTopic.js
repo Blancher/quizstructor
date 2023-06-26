a3dc2a11644733896db4171df1dcfde3c3042735
Ben Saacks <108644584+Blancher@users.noreply.github.com>
2023-06-26 13:05:56 -0700

    Update NewTopic.js

--- src/components/Topic/NewTopic.js
+++ src/components/Topic/NewTopic.js
@@ -8,13 +8,16 @@ export default function NewTopic() {
     const state = useSelector(state => state);
     const [searchParams] = useSearchParams();
     const editing = searchParams.get('editing');
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
     let name;
     if (editing === 'true') {
         name = searchParams.get('name');
     }
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+    
     let [input, valid, inputClasses, handleChange, handleBlur, handleSubmit, invalid, message] = useInput(state.map(topic => topic.name), editing === 'true' ? 'special' : 'name', 'Topic', name);
+    
     const handleFormSubmit = e => {
         e.preventDefault();
         handleSubmit();
@@ -30,7 +33,7 @@ export default function NewTopic() {
         <div className='fade'>
             <h1>{editing === 'true' ? `Editing ${name}` : 'New Topic'}</h1>
             <form onSubmit={handleFormSubmit}>
-                <Input classes={inputClasses} message={message} type='Topic' placeholder='Topic Name' onChange={handleChange} onBlur={handleBlur} invalid={invalid} defaultValue={editing === 'true' ? name : ''}/>
+                <Input classes={inputClasses} message={message} type='Topic' placeholder='Topic Name' onChange={handleChange} onBlur={handleBlur} invalid={invalid} defaultValue={editing === 'true' ? name.replace('`', ' ') : ''}/>
                 <button className='button'>{editing === 'true' ? 'Update' : 'Add'} Topic</button>
             </form>
         </div>
