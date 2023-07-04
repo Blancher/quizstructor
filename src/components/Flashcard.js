d9c35fc034ddca7f76fab98c36475167659d1b6a
Ben Saacks <108644584+Blancher@users.noreply.github.com>
2023-07-04 11:54:21 -0700

    Update Flashcard.js

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -8,6 +8,10 @@ export default function Flashcard(props) {
         <div onClick={handleClick} className='flashcard'>
             {showFront && <p className='fadeAnimation'>{props.front}</p>}
             {!showFront && <p className='fadeAnimation'>{props.back}</p>}
+            <div className='bottomLeft'>
+                {showFront && <h3 className='fadeAnimation'>Front</h3>}
+                {!showFront && <h3 className='fadeAnimation'>Back</h3>}
+            </div>
         </div>
     );
 }
